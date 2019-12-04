0141308b3f0405a991997fdbd44e54a9948dbbf3
Bernardo Esbérard <bernardo.esberard@mundipagg.com>
2019-12-04 18:02:59 -0300

    Criação do projeto

---
+++ src/Api/Controllers/HelloWorldController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers
+{
+    [ApiController]
+    [Route("hello-world")]
+    public class HelloWorldController : ControllerBase
+    {
+    }
+}
